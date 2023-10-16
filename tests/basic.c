3eb65e44fd42b0d777b9b3ead729ed1cd306b6f4
lqxc <lovebaihezi@gmail.com>
2023-10-16 23:12:24 +0800

    [test]: update test input

--- tests/basic.c
+++ tests/basic.c
@@ -4,25 +4,36 @@ int f1(int* arg1, int arg2, int arg3) {
     return y;
 }
 
-int f2(int *arg1) {
+int f2(int *arg1, int arg2) {
     if (arg1 != 0x0) {
         int x = *arg1;
-        x += 2;
+        if (arg2) {
+            x += 2;
+        }
         return x;
     } else {
         return 0;
     }
 }
 
-void f(void) {
-    int x = 1;
+void f(int init) {
+    int x = init;
     x = 2;
     x += 3;
     int y = 2;
     y = x;
-    f1(&y, f2(&x), 2);
+    int z = f2(&y, x);
+    f1(&y, z, x);
 }
 
-int main(void) {
+int main(int argc, char* args[]) {
+    switch (argc) {
+        case 0:
+            return 1;
+        case 1:
+            return 0;
+        default:
+            f(argc);
+    }
     return 0;
 }
