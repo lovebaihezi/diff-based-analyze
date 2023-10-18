1947c53b26cef18088d9043588b58e09aa39eea0
lqxc <lovebaihezi@gmail.com>
2023-10-18 23:07:57 +0800

    [build]: remove nix due to it cant fit the need

--- src/call_tree.zig
+++ src/call_tree.zig
@@ -2,7 +2,12 @@ const llvm = @import("llvm.zig");
 const CallPath = @import("call_path.zig");
 const Path = CallPath.Path;
 
-pub const FunctionNode = struct {};
+pub const FunctionNode = struct {
+    pub fn params(self: @This()) ?[]llvm.Value {
+        _ = self;
+        return null;
+    }
+};
 
 pub const BranchNode = struct {};
 
@@ -25,8 +30,13 @@ pub const CallTree = struct {
         return .{ .root = root };
     }
 
-    pub fn path(inst: llvm.Instruction) ?Path {
+    pub fn path(self: @This(), inst: llvm.Instruction) ?Path {
+        _ = self;
         _ = inst;
         return null;
+    }
+
+    pub fn next(self: @This()) ?[]const Node {
+        _ = self;
     }
 };
