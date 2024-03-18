0d396df3f19cc9773554d90c13bf76f0758da82d
Lqxc <55340837+lovebaihezi@users.noreply.github.com>
2024-03-18 10:12:39 -0400

    [feat]: more generic thread issue code (#3)

--- tests/shared_var.c
+++ tests/shared_var.c
@@ -3,40 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long shared_variable = 0;
+size_t *arrays = NULL;
+
+typedef struct {
+  size_t len;
+  size_t index;
+} Data;
 
 void *increment_thread(void *arg) {
-  int *data = (int *)arg;
-  for (int i = 0; i < *data; i++) {
-    if (shared_variable % 2 == 0) {
-      shared_variable += 2;
-    } else {
-      shared_variable = shared_variable - 1;
-    }
+  Data *data = (Data *)arg;
+  for (size_t i = 0; i < data->len; i++) {
+    arrays[i] = i + data->index % 2;
   }
+  return NULL;
+}
+
+void *init_data(void *arg) {
+  Data *data = (Data *)arg;
+  arrays = malloc(sizeof(size_t) * data->len);
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
   pthread_t threads[31];
 
-  int *data = (int *)malloc(sizeof(int));
+  Data *data = (Data *)malloc(sizeof(Data));
 
   assert(data != NULL);
 
-  *data = argc > 1 ? atoi(argv[1]) : 10;
+  data->len = argc > 1 ? atoi(argv[1]) : 10;
+  data->index = 0;
 
-  for (int i = 0; i < sizeof(threads) / sizeof(pthread_t); i += 1) {
-    pthread_create(&threads[i], NULL, increment_thread, (void *)data);
+  pthread_create(&threads[0], NULL, init_data, (void *)data);
+
+  for (size_t i = 1; i < sizeof(threads) / sizeof(pthread_t); i += 1) {
+    Data *each_data = (Data *)malloc(sizeof(Data));
+
+    assert(each_data != NULL);
+
+    each_data->len = argc > 1 ? atoi(argv[1]) : 10;
+    each_data->index = i;
+
+    pthread_create(&threads[i], NULL, increment_thread, (void *)each_data);
   }
 
-  for (int i = 0; i < sizeof(threads) / sizeof(pthread_t); i += 1) {
+  for (size_t i = 0; i < sizeof(threads) / sizeof(pthread_t); i += 1) {
     pthread_join(threads[i], NULL);
   }
 
   free(data);
+  free(arrays);
 
-  printf("%lld\n", shared_variable);
+  long long sum = 0;
+  for (size_t i = 0; i < data->len; i += 1) {
+    sum += arrays[i];
+  }
+  printf("%lld", sum);
 
   return 0;
 }
