ddf38264f69753ceffc8de06051b7b7d782e2b19
BoWen Chai <lovebaihezi@gmail.com>
2024-06-03 13:28:06 +0800

    feat: add baseline benchmark data

---
+++ tests/correct_sync/split.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+// Split stdin to stdout, output each seq in a new line.
+// Use improved KMP
+// Stdin will be seen as a string, and the spliter will be seen as a pattern.
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
+    char* spliter = argv[1];
+    size_t spliter_len = strlen(spliter);
+    size_t index = 0;
+    // KMP
+    while (1) {
+        char c = fgetc(stdin);
+        if (c == EOF) {
+            break;
+        }
+        if (c == spliter[index]) {
+            index += 1;
+            if (index == spliter_len) {
+                printf("\n");
+                index = 0;
+            }
+        } else {
+            // output spliter content and current char
+            for (size_t i = 0;i < index;i += 1) {
+                printf("%c", spliter[i]);
+            }
+            printf("%c", c);
+            // Reset index
+            index = 0;
+        }
+    }
+}
