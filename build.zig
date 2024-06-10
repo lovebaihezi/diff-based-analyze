acb371c3b1af16d43291a7aa207475f9ad5d29ed
BoWen Chai <lovebaihezi@gmail.com>
2024-06-10 15:59:47 +0800

    fixup: more verbose link info

--- build.zig
+++ build.zig
@@ -65,6 +65,7 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary2("LLVMCodeGenTypes", .{ .preferred_link_mode = .static, .needed = true });
     exe.linkSystemLibrary2("LLVMIRReader", .{ .preferred_link_mode = .static, .needed = true });
     exe.linkSystemLibrary2("LLVMIRPrinter", .{ .preferred_link_mode = .static, .needed = true });
+    exe.linkSystemLibrary2("c++abi", .{ .preferred_link_mode = .static, .needed = true });
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
