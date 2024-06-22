2cb4e57b896d5e22e48d14d7af7c87700d9c534d
BoWen Chai <lovebaihezi@gmail.com>
2024-06-22 23:46:18 +0800

    feat: clone repo, update c, cpp

--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
-import { parseFiles } from "@ast-grep/napi";
+import { parseFiles, SgRoot } from "@ast-grep/napi";
 import { writeFile } from "node:fs/promises";
-import { applyForFunctionInit } from "./rules/applyOnInitByFunction";
+import { applyForC } from "./rules/applyOnInitByFunctionForC";
+import { applyForCpp } from "./rules/applyOnInitByFunctionForC++";
 
 const main = async (paths: string[]) => {
-  await parseFiles(paths, async (err, res) => {
+  await parseFiles(paths, async (err, res: SgRoot) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
     try {
-      const modified = applyForFunctionInit(res.root());
-      await writeFile(res.filename(), "#include <pthread.h>\n" + modified);
+      if (
+        res.filename().endsWith(".cc") || res.filename().endsWith(".cpp") || res.filename().endsWith(".cu") ||
+        res.filename().endsWith(".hpp")
+      ) {
+        const modified = applyForCpp(res.root());
+        await writeFile(res.filename(), "#include <thread>\n" + modified);
+      } else if (res.filename().endsWith(".c")) {
+        const modified = applyForC(res.root());
+        await writeFile(res.filename(), "#include <pthread.h>\n" + modified);
+      } else {
+        console.warn("current not support for this file type", res.filename())
+      }
     } catch (e) {
       console.error(`failed to apply vulnerbilities for ${res.filename()}`);
     }
