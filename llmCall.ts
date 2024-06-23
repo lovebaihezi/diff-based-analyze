3336e6710524568d8812f4bff7cfd99fe980b105
BoWen Chai <lovebaihezi@gmail.com>
2024-06-23 19:46:35 +0800

    feat: script checking real problem

--- llmCall.ts
+++ llmCall.ts
@@ -4,7 +4,9 @@ import  {
   HarmBlockThreshold,
   EnhancedGenerateContentResponse,
   GenerationConfig,
+  StartChatParams,
 } from "@google/generative-ai";
+import logger from "./logger";
 
 const apiKey = process.env.GEMINI_API_KEY;
 if (!apiKey) {
@@ -16,9 +18,26 @@ const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   systemInstruction: `You are an Expert in POSIX Thread Model and Linux API, including sync prototype, atomic, CPU cache and memory corruption. You will check the input source code, point the issues including: wrong memory order of atomic variables read and write; dead lock; non-thread safe API uses in thread scope. Response in JSON format: 
 {
-  "code": $code,
-  "cwe_name": $cwe_name,
-  "sync_issue": $sync_issue
+  "codesContainsIssue": {
+    "type": "array",
+    "value": [
+      {
+        "code": {
+          "value": "...",
+          "type": "string",
+        },
+        "CWE": {
+          "value": "...",
+          "type": "string",
+        },
+        "threadSyncIssue": {
+          "value": "...",
+          "type": "string",
+        },
+      },
+      ...
+    ]
+  }
 }`
 })
 
@@ -27,15 +46,32 @@ const generationConfig: GenerationConfig = {
   topP: 0.95,
   topK: 64,
   maxOutputTokens: 8192,
-  responseMimeType: "text/plain",
+  responseMimeType: "application/json",
 };
 
-export async function checkContent(code: string): Promise<EnhancedGenerateContentResponse> {
+export function sleep(s: number): Promise<null> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null)
+    }, s * 1000)
+  })
+}
+
+interface Options extends GenerationConfig{
+  history: StartChatParams['history']
+  timeout: number
+}
+
+export async function checkContent(code: string, options?: Options): Promise<EnhancedGenerateContentResponse | null> {
   const chatSession = model.startChat({
     generationConfig,
-    history: [],
+    ...options
   });
 
-  const result = await chatSession.sendMessage(code);
-  return result.response
+  const result = await Promise.race([chatSession.sendMessage(code), sleep(options?.timeout ?? 10)]);
+  if (result) {
+    return result.response
+  }
+  logger.error("call llm timeout")
+  return null
 }
