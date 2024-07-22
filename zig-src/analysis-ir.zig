034056f7ef49ea4f078f945939fa6b1954602db1
Lqxc <55340837+lovebaihezi@users.noreply.github.com>
2024-07-22 12:40:15 -0400

    fixup: add test example show the O3 compare O0 (#40)

--- zig-src/analysis-ir.zig
+++ zig-src/analysis-ir.zig
@@ -262,8 +262,8 @@ const LL_INPUT =
     \\ !10 = !{!"llvm.loop.mustprogress"}
     \\ !11 = distinct !{!11, !10}
 ;
-test "analysis on input content, gen JSON" {
-    // Currently use Arena escape the leak check, will check why the hashMap excapsed later
+
+test "analysis on O3 input content, gen JSON" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const name: [:0]const u8 = "cat.ll";
@@ -277,7 +277,128 @@ test "analysis on input content, gen JSON" {
     try std.json.stringify(res, .{}, arr.writer());
     try std.testing.expect(arr.items.len != 0);
     try std.testing.expectEqualStrings(
-        \\{"global_var":{".str":{"read":[],"write":[]}},"function_var":{"main":{"":{"read":[],"write":[]}},"fopen":{"":{"read":[],"write":[]}},"fgetc":{"":{"read":[],"write":[]}},"fclose":{"":{"read":[],"write":[]}},"putchar":{"":{"read":[],"write":[]}}}}
+        \\{"global_var":{".str":{"read":[],"write":[]}},"function_var":{"main":{"":{"read":[],"write":[""]}},"fopen":{"":{"read":[],"write":[]}},"fgetc":{"":{"read":[],"write":[]}},"fclose":{"":{"read":[],"write":[]}},"putchar":{"":{"read":[],"write":[]}}}}
+    , arr.items);
+}
+
+const OG_LL_INPUT =
+    \\; ModuleID = 'tests/correct_sync/cat.c'
+    \\source_filename = "tests/correct_sync/cat.c"
+    \\target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
+    \\target triple = "x86_64-pc-linux-gnu"
+    \\
+    \\@.str = private unnamed_addr constant [2 x i8] c"r\00", align 1
+    \\@.str.1 = private unnamed_addr constant [3 x i8] c"%c\00", align 1
+    \\
+    \\; Function Attrs: noinline nounwind optnone sspstrong uwtable
+    \\define dso_local i32 @main(i32 noundef %0, ptr noundef %1) #0 {
+    \\  %3 = alloca i32, align 4
+    \\  %4 = alloca i32, align 4
+    \\  %5 = alloca ptr, align 8
+    \\  %6 = alloca i32, align 4
+    \\  %7 = alloca ptr, align 8
+    \\  %8 = alloca i8, align 1
+    \\  store i32 0, ptr %3, align 4
+    \\  store i32 %0, ptr %4, align 4
+    \\  store ptr %1, ptr %5, align 8
+    \\  store i32 1, ptr %6, align 4
+    \\  br label %9
+    \\
+    \\9:                                                ; preds = %37, %2
+    \\  %10 = load i32, ptr %6, align 4
+    \\  %11 = load i32, ptr %4, align 4
+    \\  %12 = icmp slt i32 %10, %11
+    \\  br i1 %12, label %13, label %40
+    \\
+    \\13:                                               ; preds = %9
+    \\  %14 = load ptr, ptr %5, align 8
+    \\  %15 = load i32, ptr %6, align 4
+    \\  %16 = sext i32 %15 to i64
+    \\  %17 = getelementptr inbounds ptr, ptr %14, i64 %16
+    \\  %18 = load ptr, ptr %17, align 8
+    \\  %19 = call noalias ptr @fopen(ptr noundef %18, ptr noundef @.str)
+    \\  store ptr %19, ptr %7, align 8
+    \\  %20 = load ptr, ptr %7, align 8
+    \\  %21 = icmp eq ptr %20, null
+    \\  br i1 %21, label %22, label %23
+    \\
+    \\22:                                               ; preds = %13
+    \\  store i32 1, ptr %3, align 4
+    \\  br label %40
+    \\
+    \\23:                                               ; preds = %13
+    \\  br label %24
+    \\
+    \\24:                                               ; preds = %30, %23
+    \\  %25 = load ptr, ptr %7, align 8
+    \\  %26 = call i32 @fgetc(ptr noundef %25)
+    \\  %27 = trunc i32 %26 to i8
+    \\  store i8 %27, ptr %8, align 1
+    \\  %28 = sext i8 %27 to i32
+    \\  %29 = icmp ne i32 %28, -1
+    \\  br i1 %29, label %30, label %34
+    \\
+    \\30:                                               ; preds = %24
+    \\  %31 = load i8, ptr %8, align 1
+    \\  %32 = sext i8 %31 to i32
+    \\  %33 = call i32 (ptr, ...) @printf(ptr noundef @.str.1, i32 noundef %32)
+    \\  br label %24, !llvm.loop !6
+    \\
+    \\34:                                               ; preds = %24
+    \\  %35 = load ptr, ptr %7, align 8
+    \\  %36 = call i32 @fclose(ptr noundef %35)
+    \\  br label %37
+    \\
+    \\37:                                               ; preds = %34
+    \\  %38 = load i32, ptr %6, align 4
+    \\  %39 = add nsw i32 %38, 1
+    \\  store i32 %39, ptr %6, align 4
+    \\  br label %9, !llvm.loop !8
+    \\
+    \\40:                                               ; preds = %22, %9
+    \\  %41 = load i32, ptr %3, align 4
+    \\  ret i32 %41
+    \\}
+    \\
+    \\declare noalias ptr @fopen(ptr noundef, ptr noundef) #1
+    \\
+    \\declare i32 @fgetc(ptr noundef) #1
+    \\
+    \\declare i32 @printf(ptr noundef, ...) #1
+    \\
+    \\declare i32 @fclose(ptr noundef) #1
+    \\
+    \\attributes #0 = { noinline nounwind optnone sspstrong uwtable "frame-pointer"="all" "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\attributes #1 = { "frame-pointer"="all" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\
+    \\!llvm.module.flags = !{!0, !1, !2, !3, !4}
+    \\!llvm.ident = !{!5}
+    \\
+    \\!0 = !{i32 1, !"wchar_size", i32 4}
+    \\!1 = !{i32 8, !"PIC Level", i32 2}
+    \\!2 = !{i32 7, !"PIE Level", i32 2}
+    \\!3 = !{i32 7, !"uwtable", i32 2}
+    \\!4 = !{i32 7, !"frame-pointer", i32 2}
+    \\!5 = !{!"clang version 18.1.8"}
+    \\!6 = distinct !{!6, !7}
+    \\!7 = !{!"llvm.loop.mustprogress"}
+    \\!8 = distinct !{!8, !7}
+;
+test "analysis on Og input content, get json" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const name: [:0]const u8 = "cat.ll";
+    var analysis = try Analysis.initWithMem(arena.allocator(), name, OG_LL_INPUT);
+    defer analysis.deinit();
+    try analysis.run(arena.allocator());
+    var res = analysis.res;
+    defer res.deinit();
+    var arr = std.ArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+    try std.json.stringify(res, .{}, arr.writer());
+    try std.testing.expect(arr.items.len != 0);
+    try std.testing.expectEqualStrings(
+        \\{"global_var":{".str.1":{"read":[],"write":[]}},"function_var":{"main":{"":{"read":["","","","","","","",""],"write":["","","","","","","","","","",""]}}}}
     , arr.items);
 }
 
