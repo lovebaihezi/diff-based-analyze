7cffed7dc9d63ed67c056d56e64a20bf1a8439aa
Lqxc <55340837+lovebaihezi@users.noreply.github.com>
2024-07-28 12:59:46 -0400

    feat: Compile by compile_commands.json (#41)

--- zig-src/ir-analyzer.zig
+++ zig-src/ir-analyzer.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 const AnalysisIR = @import("analysis-ir.zig");
+const compile2ir = @import("compile2ir.zig");
 
 const Allocator = std.mem.Allocator;
 
-pub fn analyze_compile_commands(self: *@This(), allocator: Allocator, json_path: []const u8) !void {
+pub fn analyze_compile_commands(self: *@This(), cwd: std.fs.Dir, allocator: Allocator, json_path: []const u8) !void {
     _ = self;
-    _ = allocator;
-    _ = json_path;
+    const paths = try compile2ir.fromCompileCommands(cwd, allocator, json_path);
+    for (paths) |path| {
+        // Create Membuf for analyzing
+        const buf = try allocator.dupeZ(u8, path);
+        allocator.free(path);
+        defer allocator.free(buf);
+        var analysis = try AnalysisIR.initWithFile(allocator, buf);
+        try analysis.run(allocator);
+    }
+    // TODO: collect the res
 }
