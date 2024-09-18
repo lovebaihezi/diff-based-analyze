8aff08bd609dcd390a32ac987559f9b28ed3ee7b
BoWen Chai <lovebaihezi@gmail.com>
2024-09-18 11:11:29 +0800

    fixup: using O3 optimized result

--- src/var-changes.zig
+++ src/var-changes.zig
@@ -9,58 +9,41 @@ const before =
     \\
     \\@.str = private unnamed_addr constant [5 x i8] c"%zu\0A\00", align 1
     \\
-    \\; Function Attrs: noinline nounwind optnone sspstrong uwtable
-    \\define dso_local i32 @main(i32 noundef %0, ptr noundef %1) #0 {
-    \\  %3 = alloca i32, align 4
-    \\  %4 = alloca i32, align 4
-    \\  %5 = alloca ptr, align 8
-    \\  %6 = alloca i64, align 8
-    \\  store i32 0, ptr %3, align 4
-    \\  store i32 %0, ptr %4, align 4
-    \\  store ptr %1, ptr %5, align 8
-    \\  store i64 1, ptr %6, align 8
-    \\  br label %7
+    \\; Function Attrs: nofree nounwind sspstrong uwtable
+    \\define dso_local noundef i32 @main(i32 noundef %0, ptr nocapture noundef readnone %1) local_unnamed_addr #0 {
+    \\  %3 = sext i32 %0 to i64
+    \\  %4 = icmp ugt i32 %0, 1
+    \\  br i1 %4, label %6, label %5
     \\
-    \\7:                                                ; preds = %15, %2
-    \\  %8 = load i64, ptr %6, align 8
-    \\  %9 = load i32, ptr %4, align 4
-    \\  %10 = sext i32 %9 to i64
-    \\  %11 = icmp ult i64 %8, %10
-    \\  br i1 %11, label %12, label %18
-    \\
-    \\12:                                               ; preds = %7
-    \\  %13 = load i64, ptr %6, align 8
-    \\  %14 = call i32 (ptr, ...) @printf(ptr noundef @.str, i64 noundef %13)
-    \\  br label %15
-    \\
-    \\15:                                               ; preds = %12
-    \\  %16 = load i64, ptr %6, align 8
-    \\  %17 = add i64 %16, 1
-    \\  store i64 %17, ptr %6, align 8
-    \\  br label %7, !llvm.loop !6
-    \\
-    \\18:                                               ; preds = %7
+    \\5:                                                ; preds = %6, %2
     \\  ret i32 0
+    \\
+    \\6:                                                ; preds = %2, %6
+    \\  %7 = phi i64 [ %9, %6 ], [ 1, %2 ]
+    \\  %8 = tail call i32 (ptr, ...) @printf(ptr noundef nonnull dereferenceable(1) @.str, i64 noundef %7)
+    \\  %9 = add nuw i64 %7, 1
+    \\  %10 = icmp eq i64 %9, %3
+    \\  br i1 %10, label %5, label %6, !llvm.loop !5
     \\}
     \\
-    \\declare i32 @printf(ptr noundef, ...) #1
+    \\; Function Attrs: nofree nounwind
+    \\declare noundef i32 @printf(ptr nocapture noundef readonly, ...) local_unnamed_addr #1
     \\
-    \\attributes #0 = { noinline nounwind optnone sspstrong uwtable "frame-pointer"="all" "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
-    \\attributes #1 = { "frame-pointer"="all" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\attributes #0 = { nofree nounwind sspstrong uwtable "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\attributes #1 = { nofree nounwind "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
     \\
-    \\!llvm.module.flags = !{!0, !1, !2, !3, !4}
-    \\!llvm.ident = !{!5}
+    \\!llvm.module.flags = !{!0, !1, !2, !3}
+    \\!llvm.ident = !{!4}
     \\
     \\!0 = !{i32 1, !"wchar_size", i32 4}
     \\!1 = !{i32 8, !"PIC Level", i32 2}
     \\!2 = !{i32 7, !"PIE Level", i32 2}
     \\!3 = !{i32 7, !"uwtable", i32 2}
-    \\!4 = !{i32 7, !"frame-pointer", i32 2}
-    \\!5 = !{!"clang version 18.1.8"}
-    \\!6 = distinct !{!6, !7}
-    \\!7 = !{!"llvm.loop.mustprogress"}
+    \\!4 = !{!"clang version 18.1.8"}
+    \\!5 = distinct !{!5, !6}
+    \\!6 = !{!"llvm.loop.mustprogress"}
 ;
-const after =
+const after = 
     \\; ModuleID = '/home/I/projects/diff-based-analyze/challenges-a/file-content-changes/variable-rename/after.c'
     \\source_filename = "/home/I/projects/diff-based-analyze/challenges-a/file-content-changes/variable-rename/after.c"
     \\target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
@@ -68,56 +51,39 @@ const after =
     \\
     \\@.str = private unnamed_addr constant [5 x i8] c"%zu\0A\00", align 1
     \\
-    \\; Function Attrs: noinline nounwind optnone sspstrong uwtable
-    \\define dso_local i32 @main(i32 noundef %0, ptr noundef %1) #0 {
-    \\  %3 = alloca i32, align 4
-    \\  %4 = alloca i32, align 4
-    \\  %5 = alloca ptr, align 8
-    \\  %6 = alloca i64, align 8
-    \\  store i32 0, ptr %3, align 4
-    \\  store i32 %0, ptr %4, align 4
-    \\  store ptr %1, ptr %5, align 8
-    \\  store i64 1, ptr %6, align 8
-    \\  br label %7
+    \\; Function Attrs: nofree nounwind sspstrong uwtable
+    \\define dso_local noundef i32 @main(i32 noundef %0, ptr nocapture noundef readnone %1) local_unnamed_addr #0 {
+    \\  %3 = sext i32 %0 to i64
+    \\  %4 = icmp ugt i32 %0, 1
+    \\  br i1 %4, label %6, label %5
     \\
-    \\7:                                                ; preds = %15, %2
-    \\  %8 = load i64, ptr %6, align 8
-    \\  %9 = load i32, ptr %4, align 4
-    \\  %10 = sext i32 %9 to i64
-    \\  %11 = icmp ult i64 %8, %10
-    \\  br i1 %11, label %12, label %18
-    \\
-    \\12:                                               ; preds = %7
-    \\  %13 = load i64, ptr %6, align 8
-    \\  %14 = call i32 (ptr, ...) @printf(ptr noundef @.str, i64 noundef %13)
-    \\  br label %15
-    \\
-    \\15:                                               ; preds = %12
-    \\  %16 = load i64, ptr %6, align 8
-    \\  %17 = add i64 %16, 1
-    \\  store i64 %17, ptr %6, align 8
-    \\  br label %7, !llvm.loop !6
-    \\
-    \\18:                                               ; preds = %7
+    \\5:                                                ; preds = %6, %2
     \\  ret i32 0
+    \\
+    \\6:                                                ; preds = %2, %6
+    \\  %7 = phi i64 [ %9, %6 ], [ 1, %2 ]
+    \\  %8 = tail call i32 (ptr, ...) @printf(ptr noundef nonnull dereferenceable(1) @.str, i64 noundef %7)
+    \\  %9 = add nuw i64 %7, 1
+    \\  %10 = icmp eq i64 %9, %3
+    \\  br i1 %10, label %5, label %6, !llvm.loop !5
     \\}
     \\
-    \\declare i32 @printf(ptr noundef, ...) #1
+    \\; Function Attrs: nofree nounwind
+    \\declare noundef i32 @printf(ptr nocapture noundef readonly, ...) local_unnamed_addr #1
     \\
-    \\attributes #0 = { noinline nounwind optnone sspstrong uwtable "frame-pointer"="all" "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
-    \\attributes #1 = { "frame-pointer"="all" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\attributes #0 = { nofree nounwind sspstrong uwtable "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
+    \\attributes #1 = { nofree nounwind "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
     \\
-    \\!llvm.module.flags = !{!0, !1, !2, !3, !4}
-    \\!llvm.ident = !{!5}
+    \\!llvm.module.flags = !{!0, !1, !2, !3}
+    \\!llvm.ident = !{!4}
     \\
     \\!0 = !{i32 1, !"wchar_size", i32 4}
     \\!1 = !{i32 8, !"PIC Level", i32 2}
     \\!2 = !{i32 7, !"PIE Level", i32 2}
     \\!3 = !{i32 7, !"uwtable", i32 2}
-    \\!4 = !{i32 7, !"frame-pointer", i32 2}
-    \\!5 = !{!"clang version 18.1.8"}
-    \\!6 = distinct !{!6, !7}
-    \\!7 = !{!"llvm.loop.mustprogress"}
+    \\!4 = !{!"clang version 18.1.8"}
+    \\!5 = distinct !{!5, !6}
+    \\!6 = !{!"llvm.loop.mustprogress"}
 ;
 
 test "variable got renamed" {
