9ba1eeebdcf13ff840f6af4472dc0a949f014639
BoWen Chai <lovebaihezi@gmail.com>
2024-10-07 09:14:00 -0400

    fixup

--- challenges-a/src/file-content-changes/variable-rename/after.c
+++ challenges-a/src/file-content-changes/variable-rename/after.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-size_t i;
-
 int main(int argc, char* argv[]) {
-  for (i = 1;i < argc;i += 1) {
+  for (size_t i = 1;i < argc;i += 1) {
     printf("%zu\n", i);
   }
   return 0;
--- challenges-a/src/file-content-changes/variable-rename/before.c
+++ challenges-a/src/file-content-changes/variable-rename/before.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-size_t arg_index;
-
 int main(int argc, char* argv[]) {
-  for (arg_index = 1;arg_index < argc;arg_index += 1) {
+  for (size_t arg_index = 1;arg_index < argc;arg_index += 1) {
     printf("%zu\n", arg_index);
   }
   return 0;
