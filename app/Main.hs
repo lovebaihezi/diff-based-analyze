c4cce9ec467c508e19671c31cbc21de51ef084bb
BoWen Chai <lovebaihezi@gmail.com>
2024-10-13 19:08:53 +0800

    fixup: try 2/N

--- app/Main.hs
+++ app/Main.hs
@@ -9,22 +9,20 @@ import qualified LLVM.AST.Operand as O
 findDbgValueCalls :: Module -> [Instruction]
 findDbgValueCalls m = concatMap findInFunction (moduleDefinitions m)
   where
-    findInFunction (GlobalDefinition f@(Function _ _ _ _ _ _ _ _ _ _ _ _ _ _ blocks _ _)) =
+    findInFunction (GlobalDefinition (Function _ _ _ _ _ _ _ _ _ _ _ _ _ _ blocks _ _)) =
       concatMap findInBlock blocks
     findInFunction _ = []
 
     findInBlock (BasicBlock _ instrs _) =
-      filter isDbgValueCall instrs
 
-    isDbgValueCall (Call _ _ (O.ConstantOperand (GlobalReference _ name)) _ _ _ datameta) =
-      name == Name "llvm.dbg.value"
+    isDbgValueCall :: Instruction -> Bool
     isDbgValueCall _ = False
 
-analyzeDbgValueCall :: Instruction -> Maybe (O.Operand, O.Operand, O.Operand)
-analyzeDbgValueCall (Call _ _ _ _ _ metadata _) =
-  case args of
-    [value, variable, expression] -> Just (value, variable, expression)
-    _ -> Nothing
+-- analyzeDbgValueCall :: Instruction -> Maybe (O.Operand, O.Operand, O.Operand)
+-- analyzeDbgValueCall (Call _ _ _ _ _ metadata inst_metadata) =
+--   case args of
+--     [value, variable, expression] -> Just (value, variable, expression)
+--     _ -> Nothing
 analyzeDbgValueCall _ = Nothing
 
 main :: IO ()
