eaac2f72606d40a7da356544eb28d050b81ccdc8
BoWen Chai <lovebaihezi@gmail.com>
2024-11-09 22:35:34 +0800

    fixup: better example

--- analysis/VariableAppTest.cpp
+++ analysis/VariableAppTest.cpp
@@ -53,9 +53,7 @@ TEST_CASE("Run APP on only name changed IR file", "[App, LLVM][.]") {
   auto diffs = current - previous;
 
   REQUIRE(diffs.getNameChanges().size() == 1);
-  REQUIRE(diffs.getChangedVariablesNames().size() == 1);
-  REQUIRE(diffs.getAdds().size() == 0);
-  REQUIRE(diffs.getRemoves().size() == 0);
+  REQUIRE(diffs.getChangedVariablesNames().size() == 0);
 
   app->shutdown();
 }
@@ -85,12 +83,14 @@ TEST_CASE("APP Test Case: check the bounded inst changes", "[App, LLVM]") {
   auto rev_diffs = checked - unchecked;
 
   REQUIRE(rev_diffs.getNameChanges().size() == 0);
+  REQUIRE(rev_diffs.getChangedVariablesNames().size() == 0);
   REQUIRE(rev_diffs.getAdds().size() == 2);
   REQUIRE(rev_diffs.getRemoves().size() == 0);
 
   auto diffs = unchecked - checked;
 
   REQUIRE(diffs.getNameChanges().size() == 0);
+  REQUIRE(rev_diffs.getChangedVariablesNames().size() == 0);
   REQUIRE(diffs.getAdds().size() == 0);
   REQUIRE(diffs.getRemoves().size() == 2);
 
--- tests/missed_bound_check/unchecked.c
+++ tests/missed_bound_check/unchecked.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum Kind {
-  A,
-  B,
-  C
-};
+enum Kind { A, B, C };
 
 struct Param {
   enum Kind kind;
   int value;
 };
 
-const char* functionA(struct Param* input, int flag) {
-  if (flag && input) {
-    char* buf = (char*)malloc(sizeof(char) + sizeof(int) + 1);
+const char *functionA(struct Param *input, int flag) {
+  if (input && flag) {
+    char *buf = (char *)malloc(sizeof(char) + sizeof(int) + 1);
     switch (input->kind) {
-      case A:
-        sprintf(buf, "A%d", input->value);
-      case B:
-        sprintf(buf, "B%d", input->value);
-      case C:
-        sprintf(buf, "C%d", input->value);
+    case A:
+      sprintf(buf, "A%d", input->value);
+    case B:
+      sprintf(buf, "B%d", input->value);
+    case C:
+      sprintf(buf, "C%d", input->value);
     }
     return buf;
   } else {
     switch (input->kind) {
-      case A:
-        return "A";
-      case B:
-        return "B";
-      case C:
-        return "C";
+    case A:
+      return "A";
+    case B:
+      return "B";
+    case C:
+      return "C";
     }
   }
 }
